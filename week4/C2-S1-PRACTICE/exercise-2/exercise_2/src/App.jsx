b4962d0d657284a091a21a04c53957de21bc62af
unknown <norakvannada.din@student.cadt.edu.kh>
2026-02-05 20:55:21 +0700

    week4

---
+++ week4/C2-S1-PRACTICE/exercise-2/exercise_2/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Header from './component/Header'
+import Main from './component/Main'
+import Footer from './component/Footer' 
+
+
+function App() {
+return (
+  <>
+    <Header />
+    <hr />
+    <Main />
+    <Footer />
+  </>);
+}
+export default App;
